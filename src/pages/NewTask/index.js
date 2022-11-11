377d3e00cd34bfd2c6bcd703f93b23df9d446d01
Gabriel Novaes <gabrielnovaes@yahoo.com.br>
2022-11-11 11:45:10 -0300

    realizado melhoria ao adcionar nova tarefa

--- src/pages/NewTask/index.js
+++ src/pages/NewTask/index.js
@@ -20,8 +20,7 @@ export function NewTask({navigation }) {
         }).then(() => {
             setNewTask('')
             navigation.navigate('Task')
-        })
-       
+        })   
     }
 
     return (
