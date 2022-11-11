43ef6597fcb4392c674a72123a234e39bb8f1765
Gabriel Novaes <gabrielnovaes@yahoo.com.br>
2022-11-11 11:41:38 -0300

    criado fluxo de adcionar nova tarefa

--- src/pages/NewTask/index.js
+++ src/pages/NewTask/index.js
@@ -1,55 +1,75 @@
 import { useState } from "react";
-import { StyleSheet, Text, View,TextInput,TouchableOpacity } from "react-native";
+import { StyleSheet, Text, View, TextInput, TouchableOpacity, Alert } from "react-native";
 import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
 import { faSave } from '@fortawesome/free-solid-svg-icons/faSave'
+import firebase from '../../config/firebase.config'
 
-export function NewTask({route,navigation}){
+export function NewTask({navigation }) {
 
-    const [newTask,setNewTask] = useState([])
-  
+    const [newTask, setNewTask] = useState('')
 
-    return(
+
+    function addTask() {
+        if(newTask===''){
+            alert("Preencha o campo descrição de tarefas")
+            return
+        }
+         firebase.firestore().collection("tasks").add({
+            description: newTask,
+            status: false
+        }).then(() => {
+            setNewTask('')
+            navigation.navigate('Task')
+        })
+       
+    }
+
+    return (
         <View style={styles.container}>
-           <TextInput placeholder="Digite uma descrição da Tarefa" style={styles.inputDescription} />
+            <TextInput  value={newTask}
+                onChangeText={text => setNewTask(text)} 
+                placeholder="Digite uma descrição da Tarefa" 
+                style={styles.inputDescription} />
 
-           <TouchableOpacity 
+            <TouchableOpacity
                 style={styles.buttonNewTask}
-                onPress={() => navigation.navigate("Task") }>
-                 <FontAwesomeIcon style={styles.iconButton} icon={faSave} size={24} name='save'></FontAwesomeIcon> 
-             </TouchableOpacity>
+                onPress={addTask}>
+                <FontAwesomeIcon style={styles.iconButton} icon={faSave} size={24} name='save'></FontAwesomeIcon>
+            </TouchableOpacity>
+
         </View>
     )
 }
 
 const styles = StyleSheet.create({
-    container:{
-        flex:1,
-        padding:8,
+    container: {
+        flex: 1,
+        padding: 8,
     },
-    inputDescription:{
-        height:56,
-        padding:16,
+    inputDescription: {
+        height: 56,
+        padding: 16,
         borderColor: '#f1f1f1',
-        borderWidth:1,
+        borderWidth: 1,
         backgroundColor: '#fff',
-        borderRadius:8,
+        borderRadius: 8,
 
     },
-    buttonNewTask:{
-        bottom:30,
-        left:20,
-        width:60,
-        height:60,
-        borderRadius:50,
-        backgroundColor:'#f92e6a',
-        justifyContent:"center",
+    buttonNewTask: {
+        bottom: 30,
+        left: 20,
+        width: 60,
+        height: 60,
+        borderRadius: 50,
+        backgroundColor: '#f92e6a',
+        justifyContent: "center",
         alignItems: "center",
-        position:"absolute",
-        shadowColor:'#888',
-        elevation:16,
-        shadowRadius:10
+        position: "absolute",
+        shadowColor: '#888',
+        elevation: 16,
+        shadowRadius: 10
     },
-    iconButton:{
-        color:'#fff',
+    iconButton: {
+        color: '#fff',
     },
 })
